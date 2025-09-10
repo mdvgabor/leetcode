e4cff876618553fa8e6f3b823d5a9f49e007977d
Medvegy Gábor <medvegygabor@Gbor-MacBook-Air.local>
2025-09-10 12:12:37 +0200

    summary_ranges.py feltöltése.

---
+++ summary_ranges.py
@@ -0,0 +1,22 @@
+def summary_ranges(nums: list):
+    n = len(nums)
+    answer = []
+    i = 0
+
+    while i < n:
+        j = i
+        while j + 1 < n and nums[j + 1] == nums[j] + 1:
+            j += 1
+        
+        if i == j:
+            answer.append(str(nums[i]))
+        else:
+            answer.append(str(nums[i]) + "->" + str(nums[j]))
+        
+        i = j + 1
+    
+    return answer
+
+
+print(summary_ranges([0, 1, 2, 4, 5, 7]))
+# ['0->2', '4->5', '7']
